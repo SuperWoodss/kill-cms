eca10642de485905104888460782ee35fab42b75
superwoods <st_sister@icloud.com>
2016-08-09 18:55:08 +0800

    Updates base configs

---
+++ js/kill-cms.js
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) Xinhuanet Inc. All rights reserved.
+ *
+ * @License: MIT
+ * @Author: SuperWoods
+ * @Email:  st_sister@iCloud.com
+ * @Date:   2016-08-09-06:45:42
+ *
+ * @(demo)Last modified by:   SuperWoods
+ * @(demo)Last modified time: 2016-08-09-06:54:32
+ */
+
+$(() => {
+    console.log('test');
+});
