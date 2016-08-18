b546db2853dee1f11c3b87b7e4e60fc4c4566ffe
superwoods <st_sister@icloud.com>
2016-08-18 13:07:51 +0800

    Updates & Add HTMLformat modules

--- js/kill-cms.js
+++ js/kill-cms.js
@@ -7,7 +7,7 @@
  * @Date:   2016-08-09-06:45:42
  *
  * @(demo)Last modified by:   SuperWoods
- * @(demo)Last modified time: 2016-08-17-05:01:27
+ * @(demo)Last modified time: 2016-08-18-01:06:44
  */
 
 $(() => {
@@ -22,7 +22,21 @@ $(() => {
         $tempShow.text(DOM);
     }
 
+    // attr
     let attr = '';
+    let $attr = $('#attr');
+    let $attrText = $attr.find('.attr');
+    let $nameAttrArray = $attr.find('[name="attr"]');
+
+    let $format = $('#format');
+    let $formatInput = $format.find('input');
+    let n = '\n';
+    $formatInput.eq(0).on('click', (e) => {
+        n = '\n';
+    });
+    $formatInput.eq(1).on('click', (e) => {
+        n = '';
+    });
 
     // killHandler
     let killHandler = ($tempDOM) => {
@@ -31,10 +45,6 @@ $(() => {
         let repeat = $('#repeat').val();
         let dayspan = (repeat < 200) ? 0 : repeat;
 
-        let nOn = 1; // 0: false, 1: tre\ue;
-        let n = (nOn) ? '\n' : '';
-
-        // let n = '';
         let cms = {
             begin: '<!--webbot bot="AdvTitleList" nodeid="' + ((nodeid === '') ? '888888' : nodeid) + '" type="0" spanmode="0" dayspan="' + dayspan + '" attr="' + attr + '" comstring="' + n,
             lt: '&lt;',
@@ -67,30 +77,36 @@ $(() => {
 
         // 处理 a 内部内容
         $a.each((i, e) => {
-            var _this = $(e);
-            if (_this.find('img').length === 0) {
-                _this.text(cms.Title);
+            let $this = $(e);
+
+            if ($this.find('img').length === 0) {
+                $this.text(cms.Title);
             }
+            // else {
+            //     let html = $.trim($(e).html());
+            //     console.log(html);
+            //     $this.html(html);
+            // }
         });
 
         // 处理 $abs
         $abs.each((i, e) => {
-            let _this = $(e);
-            if (_this.find('a')) {
-                let aText = _this.find('a').text();
-                _this.text(cms.Abstract + 'URL_BEGIN<a href="' + cms.ArticleUrlPh + '">URL_END' + aText + '</a>');
+            let $this = $(e);
+            if ($this.find('a')) {
+                let aText = $this.find('a').text();
+                $this.text(cms.Abstract + 'URL_BEGIN<a href="' + cms.ArticleUrlPh + '">URL_END' + aText + '</a>');
             } else {
-                _this.text(cms.Abstract);
+                $this.text(cms.Abstract);
             }
         });
 
         // 处理 $subTitle
         $subTitle.each((i, e) => {
-            let _this = $(e);
-            if (_this.find('a')) {
-                _this.text('URL_BEGIN<a href="' + cms.ArticleUrlPh + '">URL_END' + cms.Subtitle + '</a>');
+            let $this = $(e);
+            if ($this.find('a')) {
+                $this.text('URL_BEGIN<a href="' + cms.ArticleUrlPh + '">URL_END' + cms.Subtitle + '</a>');
             } else {
-                _this.text(cms.Subtitle);
+                $this.text(cms.Subtitle);
             }
         });
 
@@ -99,12 +115,12 @@ $(() => {
 
         // 处理 img
         $img.each((i, e) => {
-            let _this = $(e);
-            _this.replaceWith(() => {
+            let $this = $(e);
+            $this.replaceWith(() => {
                 let temp = cms.Picture.needcode[2];
-                if (_this.attr('data-src')) {
+                if ($this.attr('data-src')) {
                     temp = cms.Picture.needcode[0]
-                } else if (_this.attr('data-original')) {
+                } else if ($this.attr('data-original')) {
                     temp = cms.Picture.needcode[1]
                 }
                 return temp;
@@ -113,6 +129,15 @@ $(() => {
 
         //
         let orgHtml = $tempDOM.html();
+
+        // demo:
+        orgHtml = $.HTMLFormat(orgHtml, {
+            indent_size: (n === '\n') ? 4 : 0,
+            wrap: (n === '\n') ? true : false,
+            indent_character: (n === '\n') ? ' ' : '',
+            // max_char: 0x10000000
+        });
+        console.log('HTMLFormat orgHtml: ', orgHtml);
 
         // 开发用模块：查看处理完的 DOM
         showDOM(orgHtml);
@@ -158,15 +183,17 @@ $(() => {
             orgHtml + n +
             cms.end;
 
+        console.log(outputTemp);
+
         $output.text(outputTemp);
     }
 
     // attrHandler
-    let $attr = $('#attr');
-    let $attrText = $attr.find('.attr');
-    let $nameAttrArray = $attr.find('[name="attr"]');
-
+    // 初始化激活默认
+    $nameAttrArray.eq(0)[0].checked = true;
     let attrHandler = (e) => {
+        $nameAttrArray.eq(0)[0].checked = false;
+
         let $this = $(e.currentTarget);
         let v = '+' + $this.val();
         console.log(v);
@@ -174,23 +201,34 @@ $(() => {
         console.log($this[0].checked && v !== '+');
 
         if ($this[0].checked && v !== '+') {
-            attr += v;
-            console.log('attr:', attr);
+            $this.attr('data-attr', v);
         } else {
-            // let reg = new RegExp(eval('/' + v + '/g'));
-            // console.log(reg);
-            attr = attr.replace(eval(v) + '/g', '');
+            $this.attr('data-attr', '');
         }
-
-        if (attr !== '' || v !== '+') {
-            $attrText.text(attr);
-        } else {
-            $attrText.text('默认');
-        }
+        //
+        // if (attr !== '' || v !== '+') {
+        //     $attrText.text(attr);
+        // } else {
+        //     $attrText.text('默认');
+        // }
     }
 
     $nameAttrArray.on('change', (e) => {
-        attrHandler(e);
+        let $this = $(e.currentTarget);
+        if ($this.val() !== '') {
+            attrHandler(e);
+        } else { // 点击默认列表关闭其它
+            $nameAttrArray
+                .attr('data-attr', '')
+                .each((i, e) => {
+                    let $this = $(e);
+                    if (i === 0) {
+                        $this[0].checked = true;
+                    } else {
+                        $this[0].checked = false;
+                    }
+                });
+        }
     });
 
     let $submit = $('#submit');
@@ -199,9 +237,25 @@ $(() => {
         let $input = $('#input');
         let inputVal = $input.val();
         let $tempDOM = $('#tempDOM');
+        // 隐藏临时DOM
+        // $tempDOM.hide();
 
         // 克隆 input 内容
         cloneDOM($tempDOM, $.trim(inputVal));
+
+
+        // 收集 attr
+        attr = '';
+        $('#attr')
+            .find('input')
+            .each((i, e) => {
+                let v = $(e).attr('data-attr');
+                console.log(v);
+                if (v) {
+                    attr += v;
+                }
+            });
+        console.log('attr:', attr);
 
         // 转译 tempDOM 并输出
         killHandler($tempDOM);
